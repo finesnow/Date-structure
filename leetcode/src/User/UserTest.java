033f7ef541cd6006fd0693307fc5659dfae3a374
dme <dme@17376>
2025-10-05 21:18:04 +0800

    2025-10-5 user类编写完成

---
+++ leetcode/src/User/UserTest.java
@@ -0,0 +1,13 @@
+package User;
+
+/**
+ * @author Administrator
+ * @date 2025/10/5 21:17
+ * @see
+ * @since JDK 17
+ */
+public class UserTest {
+    private String username;
+    private int userid;
+
+}
