960b2a3e361686ae2fa4b434774a3df7400a5e11
dme <dme@17376>
2025-10-05 22:15:22 +0800

    2025-10-5 UserService

---
+++ leetcode/src/service/UserService.java
@@ -0,0 +1,15 @@
+package service;
+
+/**
+ * @author Administrator
+ * @date 2025/10/5 22:14
+ * @see
+ * @since JDK 17
+ */
+public class UserService {
+
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
